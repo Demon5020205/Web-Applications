24b5a09ec517d131de4681d7c211604f614371cb
Demon2050502 <aleksei2050502@mail.ru>
2025-04-20 19:58:22 +0300

    фикс интревеса на структуру

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"log"
 
+	todo "example.com/mod"
 	"example.com/mod/pkg/handler"
 	"example.com/mod/pkg/repository"
 	"example.com/mod/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error  running http server: %s", err.Error)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
